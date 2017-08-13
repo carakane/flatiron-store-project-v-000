beb6565ffd7f4b5697afbb77a8e559bfd240faa6
jessica fletcher <cara.kane@gmail.com>
2017-08-13 18:14:21 +0000

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,36 @@
 class Cart < ActiveRecord::Base
+  belongs_to :user
+  has_many :line_items
+  has_many :items, through: :line_items
 
+
+  def total
+    x = 0
+    self.items.each do |item|
+      x += item.price
+    end
+    x
+  end
+
+  def add_item(item)
+    # if self.line_items.include?(item_id: item)
+    #   self.line_items.quantity += 1
+    # else
+    #   self.line_items.new(item_id: item)
+    # end
+    if LineItem.where(cart_id: self.id, item_id: item).present?
+      # binding.pry
+      a = LineItem.where(cart_id: self.id, item_id: item)
+      a.first.quantity += 1
+      # a.first.save
+      a.first
+    else
+      self.line_items.new(item_id: item)
+    end
+  end
 end
+
+#
+# .find_or_create_by
+# .includes
+# LineItem.where(cart_id: c.id)
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many :carts
+  belongs_to :current_cart, class_name: 'Cart', foreign_key: 'cart_id'
+
+end
